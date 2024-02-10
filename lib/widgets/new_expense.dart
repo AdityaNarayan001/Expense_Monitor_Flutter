c42c5705c7b6c13769270d66e18a6270b265aa35
Aditya Narayan <adityaan277@gmail.com>
2024-02-10 16:00:50 +0530

    added button on modal and amount field

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -10,12 +10,13 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
-
   final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
 
   @override
   void dispose() {
     _titleController.dispose();
+    _amountController.dispose();
     super.dispose();
   }
 
@@ -32,11 +33,24 @@ class _NewExpenseState extends State<NewExpense> {
               label: Text('Title'),
             ),
           ),
+          TextField(
+            keyboardType: TextInputType.number,
+            controller: _amountController,
+            decoration: const InputDecoration(
+              prefixText: '\$ ',
+              label: Text('Amount'),
+            ),
+          ),
           Row(
             children: [
+              TextButton(
+                onPressed: () {},
+                child: const Text('Cancel'),
+              ),
               ElevatedButton(
                   onPressed: () {
                     print(_titleController.text);
+                    print(_amountController.text);
                   },
                   child: const Text('Save Expense')),
             ],
