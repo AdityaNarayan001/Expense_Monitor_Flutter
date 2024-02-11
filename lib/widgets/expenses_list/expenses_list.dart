0eaaab2dcbde9e2f19bbf65e2868c74004ac607e
Aditya Narayan <adityaan277@gmail.com>
2024-02-11 19:27:19 +0530

    fixed modal sheet and inlined data with swipe feature on expense card

--- lib/widgets/expenses_list/expenses_list.dart
+++ lib/widgets/expenses_list/expenses_list.dart
@@ -3,15 +3,22 @@ import 'package:third_app/models/expense.dart';
 import 'package:third_app/widgets/expenses_list/expense_item.dart';
 
 class ExpensesList extends StatelessWidget {
-  const ExpensesList({super.key, required this.expenses});
+  const ExpensesList({super.key, required this.expenses, required this.onRemoveExpense,});
 
+  final void Function(Expense expense) onRemoveExpense;
   final List<Expense> expenses;
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: expenses.length,
-      itemBuilder: (ctx, index) => ExpenseItem(expenses[index]),
+      itemBuilder: (ctx, index) => Dismissible(
+        key: ValueKey(expenses[index]),
+        onDismissed: (direction) {
+          onRemoveExpense(expenses[index]);
+        },
+        child: ExpenseItem(expenses[index]),
+      ),
     );
   }
 }
